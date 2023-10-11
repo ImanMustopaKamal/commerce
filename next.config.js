97fedc247d37c23552a9f23749bdf69febee5d21
iman mustopa kamal <mustofa56@gmail.com>
2023-10-11 21:32:33 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -9,9 +9,9 @@ const nextConfig = withPWA({
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
+        protocol: 'https',
+        hostname: 'api-ecommerce.havidmohamad.com',
+        port: '',
         pathname: '/storage/**'
       }
     ]
